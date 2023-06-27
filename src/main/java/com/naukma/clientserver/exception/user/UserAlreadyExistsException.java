ae32176d8cee32fcdb170bde3e2ac29e2bf39dcd
Mariana Piz <marianapiz0412@gmail.com>
2023-06-27 19:05:05 +0300

    Create exception for existing user

---
+++ src/main/java/com/naukma/clientserver/exception/user/UserAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.naukma.clientserver.exception.user;
+
+public class UserAlreadyExistsException extends Exception {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
+
