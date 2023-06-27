6971a73bedb8fc4a2e6ef0709f67b0a0a8cebd17
Mariana Piz <marianapiz0412@gmail.com>
2023-06-27 21:37:46 +0300

    Implement creating Group with POST request

---
+++ src/main/java/com/naukma/clientserver/http/GroupHandler.java
@@ -0,0 +1,73 @@
+package com.naukma.clientserver.http;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.naukma.clientserver.exception.group.GroupAlreadyExistsException;
+import com.naukma.clientserver.exception.group.GroupNotFoundException;
+import com.naukma.clientserver.model.Group;
+import com.naukma.clientserver.request.GroupRequestData;
+import com.naukma.clientserver.service.GroupService;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+
+import java.io.IOException;
+
+public class GroupHandler implements HttpHandler {
+    private final GroupService groupService;
+
+    public GroupHandler(GroupService groupService) {
+        this.groupService = groupService;
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String authorizationHeader = exchange.getRequestHeaders().getFirst("Authorization");
+
+        if (!ServerUtils.isTokenValid(authorizationHeader)) {
+            ServerUtils.sendResponse(exchange, 403, "Forbidden");
+            return;
+        }
+
+        String method = exchange.getRequestMethod();
+
+        if(method.equalsIgnoreCase("POST"))
+            handlePostRequest(exchange);
+    }
+
+    private void handlePostRequest(HttpExchange exchange) throws IOException {
+        String requestBody = ServerUtils.getRequestData(exchange);
+        try {
+            int createGroupId = createGroup(requestBody);
+            String response = "Group with ID = " + createGroupId + " was created successfully!";
+            ServerUtils.sendResponse(exchange, 201, response);
+        } catch (GroupAlreadyExistsException e) {
+            e.printStackTrace();
+            ServerUtils.sendResponse(exchange, 409, e.getMessage());
+        }
+    }
+
+    private int createGroup(String requestBody) throws GroupAlreadyExistsException {
+        GroupRequestData groupRequestData;
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            groupRequestData = objectMapper.readValue(requestBody, GroupRequestData.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error deserializing request body", e);
+        }
+        String name = groupRequestData.getName();
+        String description = groupRequestData.getDescription();
+
+        Group newGroup = new Group(name, description);
+        groupService.createGroup(newGroup);
+
+        try {
+            return groupService.getGroupByName(name).getId();
+        } catch (GroupNotFoundException e) {
+            System.out.println("Can't get ID of created group.");
+        }
+        return -1;
+    }
+
+}
---
+++ src/main/java/com/naukma/clientserver/request/GroupRequestData.java
@@ -0,0 +1,9 @@
+package com.naukma.clientserver.request;
+
+import lombok.Data;
+
+@Data
+public class GroupRequestData {
+    private String name;
+    private String description;
+}
