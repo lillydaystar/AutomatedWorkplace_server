f81a6d5de32ca1a53f2c62c9179289a8a1618d87
Liliia Parashchak <liliia.parashchak@gmail.com>
2023-06-29 22:25:41 +0300

    Login tests

---
+++ src/test/java/com/naukma/clientserver/auth/LoginTest.java
@@ -0,0 +1,86 @@
+package com.naukma.clientserver.auth;
+
+import com.naukma.clientserver.exception.user.UserAlreadyExistsException;
+import com.naukma.clientserver.http.LoginHandler;
+import com.naukma.clientserver.http.Server;
+import com.naukma.clientserver.mock.MockHttpExchange;
+import com.naukma.clientserver.mock.MockUserService;
+import com.naukma.clientserver.model.User;
+import com.naukma.clientserver.service.UserService;
+import com.naukma.clientserver.utils.PasswordHasher;
+import com.sun.net.httpserver.Headers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.net.URI;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginTest {
+    private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsaWxseWRheXN0YXJAZ21haWwuY29tIiwiZXhwIjoxNjg4MTUyNDM0LCJpYXQiOjE2ODgwNjYwMzR9.Mx4ahmyWvJIT66r3A0EiiZULOfc4EDqrMBMH-JmI0Ac";
+    private static final String INVALID_TOKEN = "invalid_token";
+    private static final String LOGIN = "testUser";
+    private static final String PASSWORD = "testPassword";
+
+    private static UserService mockUserService;
+
+    @BeforeAll
+    static void setUp() throws UserAlreadyExistsException {
+        mockUserService = new MockUserService();
+        mockUserService.createUser(new User("lillydaystar@gmail.com", "ThomasTheBest"));
+        mockUserService.createUser(new User("testUser", "testPassword"));
+    }
+
+    @Test
+    void testValidToken() {
+        String authorizationHeader = "Bearer " + VALID_TOKEN;
+        assertTrue(Server.isTokenValid(authorizationHeader));
+    }
+
+    @Test
+    void testInvalidToken() {
+        String authorizationHeader = "Bearer " + INVALID_TOKEN;
+        assertFalse(Server.isTokenValid(authorizationHeader));
+    }
+
+    @Test
+    void testLoginHandlerWithValidCredentials() {
+        MockHttpExchange mockExchange = new MockHttpExchange();
+        mockExchange.setRequestMethod("POST");
+        String password = PasswordHasher.hashPassword(PASSWORD);
+        mockExchange.setRequestURI(URI.create("/login?login=" + LOGIN + "&password=" + password));
+        mockExchange.setRequestHeaders(new Headers());
+
+        ByteArrayOutputStream responseOutputStream = new ByteArrayOutputStream();
+        mockExchange.setResponseBody(responseOutputStream);
+
+        assertDoesNotThrow(() -> {
+            LoginHandler loginHandler = new LoginHandler(mockUserService);
+            loginHandler.handle(mockExchange);
+        });
+
+        assertEquals(200, mockExchange.getResponseCode());
+        assertNotNull(responseOutputStream.toString());
+    }
+
+    @Test
+    void testLoginHandlerWithInvalidCredentials() {
+        MockHttpExchange mockExchange = new MockHttpExchange();
+        mockExchange.setRequestMethod("POST");
+        mockExchange.setRequestURI(URI.create("/login?login=invalid&password=invalid"));
+        mockExchange.setRequestHeaders(new Headers());
+
+        ByteArrayOutputStream responseOutputStream = new ByteArrayOutputStream();
+        mockExchange.setResponseBody(responseOutputStream);
+
+        assertDoesNotThrow(() -> {
+            LoginHandler loginHandler = new LoginHandler(mockUserService);
+            loginHandler.handle(mockExchange);
+        });
+
+        assertEquals(401, mockExchange.getResponseCode());
+        assertEquals("Unauthorized", responseOutputStream.toString());
+    }
+
+}
---
+++ src/test/java/com/naukma/clientserver/mock/MockHttpExchange.java
@@ -0,0 +1,124 @@
+package com.naukma.clientserver.mock;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpPrincipal;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.URI;
+
+public class MockHttpExchange extends HttpExchange {
+    private String requestMethod;
+    private URI requestURI;
+    private Headers requestHeaders;
+    private int responseCode;
+    private ByteArrayOutputStream responseBody;
+
+    public MockHttpExchange() {
+        responseBody = new ByteArrayOutputStream();
+    }
+
+    @Override
+    public Headers getRequestHeaders() {
+        return requestHeaders;
+    }
+
+    @Override
+    public Headers getResponseHeaders() {
+        return new Headers();
+    }
+
+    @Override
+    public URI getRequestURI() {
+        return requestURI;
+    }
+
+    @Override
+    public String getRequestMethod() {
+        return requestMethod;
+    }
+
+    @Override
+    public HttpContext getHttpContext() {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public InputStream getRequestBody() {
+        return null;
+    }
+
+    @Override
+    public OutputStream getResponseBody() {
+        return responseBody;
+    }
+
+    @Override
+    public void sendResponseHeaders(int responseCode, long responseLength) {
+        this.responseCode = responseCode;
+    }
+
+    @Override
+    public InetSocketAddress getRemoteAddress() {
+        return null;
+    }
+
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    @Override
+    public InetSocketAddress getLocalAddress() {
+        return null;
+    }
+
+    @Override
+    public String getProtocol() {
+        return null;
+    }
+
+    @Override
+    public Object getAttribute(String name) {
+        return null;
+    }
+
+    @Override
+    public void setAttribute(String name, Object value) {
+
+    }
+
+    @Override
+    public void setStreams(InputStream i, OutputStream o) {
+
+    }
+
+    @Override
+    public HttpPrincipal getPrincipal() {
+        return null;
+    }
+
+    public void setRequestMethod(String requestMethod) {
+        this.requestMethod = requestMethod;
+    }
+
+    public void setRequestURI(URI requestURI) {
+        this.requestURI = requestURI;
+    }
+
+    public void setRequestHeaders(Headers requestHeaders) {
+        this.requestHeaders = requestHeaders;
+    }
+
+    public void setResponseBody(ByteArrayOutputStream responseBody) {
+        this.responseBody = responseBody;
+    }
+}
